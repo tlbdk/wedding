9c671ed3006ffae18cd7045eb6afbe309afb5156
Troels Liebe Bentsen <tlb@rapanden.dk>
2015-03-08 23:15:03 +0100

    Use yo to create the template instead

--- frontend/app/js/controllers.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/* Controllers */
-
-var weddingControllers = angular.module('weddingControllers', []);
-
-weddingControllers.controller('WelcomeCtrl', ['$scope', function($scope) {
-    $scope.title = "Welcome";
-}]);
---
+++ frontend/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name testApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the testApp
+ */
+angular.module('testApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
