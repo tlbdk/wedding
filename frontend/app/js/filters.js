081e67ee24da0c4f4c6c4d3f1fd1211b2d9d1239
Troels Liebe Bentsen <tlb@nversion.dk>
2015-06-09 13:48:16 +0200

    Add currency conversion for DKK

--- frontend/app/js/filters.js
+++ frontend/app/js/filters.js
@@ -2,8 +2,23 @@
 
 /* Filters */
 
-angular.module('weddingFilters', []).filter('checkmark', function() {
+var weddingFilters = angular.module('weddingFilters', []);
+
+weddingFilters.filter('checkmark', function() {
   return function(input) {
     return input ? '\u2713' : '\u2718';
   };
 });
+
+weddingFilters.filter('noCommaCurrency', ['$filter', '$locale', function(filter, locale) {
+  var currencyFilter = filter('currency');
+  var formats = locale.NUMBER_FORMATS;
+  return function(amount, currencySymbol) {
+    var value = currencyFilter(amount, currencySymbol);
+    var sep = value.indexOf(formats.DECIMAL_SEP);
+    if(amount >= 0) { 
+      return value.substring(0, sep).replace(/,/, '');
+    }
+    return value.substring(0, sep) + ')'.replace(/,/, '');
+  };
+}]);
